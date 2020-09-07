e1672cdbac275e2f1f223740beb9ab7524bd7a63
shrilekhaa <shriraghav18@gmail.com>
2020-09-07 18:22:59 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js")
+
+
+const items =["Buy Food","Cook Food","Eat Food"];
+const workItems = [];
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.set('view engine','ejs');
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list",{listTitle:day, newListItems:items});
+});
+
+app.post("/",function(req,res){
+  const item = req.body.newItem;
+
+  if(req.body.list === "Work")
+  {
+      workItems.push(item);
+      res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+    }
+  });
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started at port 3000");
+});
